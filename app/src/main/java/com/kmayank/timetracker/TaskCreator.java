75d3f5078b64926dd40e99d48c31dbc5d7c42ba6
Kumar <kmayank@f45c89c90583.ant.amazon.com>
2016-11-27 14:50:53 +0530

    Initial commit

---
+++ app/src/main/java/com/kmayank/timetracker/TaskCreator.java
@@ -0,0 +1,13 @@
+package com.kmayank.timetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_creator);
+    }
+}
